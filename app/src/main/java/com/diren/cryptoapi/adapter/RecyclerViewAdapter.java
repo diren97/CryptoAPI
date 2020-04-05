414eafe6a7e52c858bd37b9ea43a19457a5eefe5
diren97 <diren.aydin97@gmail.com>
2020-04-05 20:44:30 +0300

    first commit

---
+++ app/src/main/java/com/diren/cryptoapi/adapter/RecyclerViewAdapter.java
@@ -0,0 +1,139 @@
+package com.diren.cryptoapi.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.PictureDrawable;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.RequestBuilder;
+import com.diren.cryptoapi.R;
+import com.diren.cryptoapi.data.Crypto;
+import com.diren.cryptoapi.svgParser.GlideApp;
+import com.diren.cryptoapi.svgParser.SvgSoftwareLayerSetter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RowHolder> {
+
+    ArrayList<Crypto> cryptoList;
+    OnItemClickListener clickListener;
+    Context context;
+    private RequestBuilder<PictureDrawable> requestBuilder;
+    private static final int ITEM = 0;
+    private static final int LOADING = 1;
+
+    private boolean isLoadingAdded = false;
+
+    public RecyclerViewAdapter(Context context, OnItemClickListener clickListener) {
+        cryptoList = new ArrayList<>();
+        this.clickListener = clickListener;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.row_layout, parent, false);
+        final RowHolder holder = new RowHolder(view);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickListener.onItemClick(v, holder.getAdapterPosition());
+            }
+        });
+        return new RowHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RowHolder holder, int position) {
+        holder.bind(cryptoList.get(position), position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return cryptoList == null ? 0 :cryptoList.size();
+    }
+
+    public class RowHolder extends RecyclerView.ViewHolder {
+        TextView txtCryptoName;
+        TextView txtCrypto;
+        ImageView imgIcon;
+
+        public RowHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void bind(Crypto crypto, int position) {
+            try {
+                txtCryptoName = itemView.findViewById(R.id.txtCryptoName);
+                txtCrypto = itemView.findViewById(R.id.txtCrypto);
+                imgIcon = itemView.findViewById(R.id.imgCrypto);
+                txtCryptoName.setText(crypto.data.getCoins().get(position).getSymbol());
+                txtCrypto.setText(String.format("%.2f", crypto.getData().getCoins().get(position).getPrice()));
+                requestBuilder =
+                        GlideApp.with(context)
+                                .as(PictureDrawable.class)
+                                .transition(withCrossFade())
+                                .listener(new SvgSoftwareLayerSetter());
+
+                Uri uri = Uri.parse(crypto.getData().getCoins().get(position).getIconUrl());
+                requestBuilder.load(uri).into(imgIcon);
+                if (crypto.getData().getCoins().get(position).getColor() != null) {
+                    txtCryptoName.setTextColor(Color.parseColor(crypto.getData().getCoins().get(position).getColor().toUpperCase()));
+                    txtCrypto.setTextColor(Color.parseColor(crypto.getData().getCoins().get(position).getColor().toUpperCase()));
+                }
+            }catch (IllegalArgumentException e){
+
+            }
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return (position == cryptoList.size() - 1 && isLoadingAdded) ? LOADING : ITEM;
+    }
+
+    public void addAll(List<Crypto> mcList) {
+        cryptoList.addAll(mcList);
+        notifyItemInserted(cryptoList.size() - 1);
+    }
+
+    public void addLoadingFooter() {
+        isLoadingAdded = true;
+        //add(new CryptoAPI());
+    }
+
+    public void removeLoadingFooter() {
+        isLoadingAdded = false;
+
+        int position = cryptoList.size() - 1;
+        Crypto item = getItem(position);
+
+        if (item != null) {
+            cryptoList.remove(position);
+            notifyItemRemoved(position);
+        }
+    }
+
+    public Crypto getItem(int position) {
+        return cryptoList.get(position);
+    }
+
+
+}
---
+++ app/src/main/java/com/diren/cryptoapi/view/MainActivity.java
@@ -0,0 +1,165 @@
+package com.diren.cryptoapi.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.diren.cryptoapi.R;
+import com.diren.cryptoapi.adapter.RecyclerViewAdapter;
+import com.diren.cryptoapi.data.Crypto;
+import com.diren.cryptoapi.listener.PaginationScrollListener;
+import com.diren.cryptoapi.service.CryptoAPI;
+import com.diren.cryptoapi.service.RetrofitClient;
+import com.kaopiz.kprogresshud.KProgressHUD;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerViewAdapter recyclerViewAdapter;
+    Context context;
+    LinearLayoutManager linearLayoutManager;
+    private KProgressHUD hud;
+    private static final int PAGE_START = 0;
+    private boolean isLoading = false;
+    private boolean isLastPage = false;
+    private int TOTAL_PAGES = 4;
+    private int currentPage = PAGE_START;
+    ArrayList<Crypto> liste;
+    int succesCode = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        liste = new ArrayList<>();
+        context = this;
+        recyclerView = findViewById(R.id.recyclerView);
+        loadData(currentPage);
+        linearLayoutManager = new LinearLayoutManager(MainActivity.this);
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        recyclerView.addOnScrollListener(new PaginationScrollListener(linearLayoutManager) {
+            @Override
+            protected void loadMoreItems() {
+                isLoading = true;
+                currentPage += 1;
+                loadNextPage(currentPage);
+            }
+
+            @Override
+            public int getTotalPageCount() {
+                return TOTAL_PAGES;
+            }
+
+            @Override
+            public boolean isLastPage() {
+                return isLastPage;
+            }
+
+            @Override
+            public boolean isLoading() {
+                return isLoading;
+            }
+        });
+    }
+
+    private void showRecycler() {
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerViewAdapter = new RecyclerViewAdapter(getApplicationContext(), new RecyclerViewAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
+            }
+        });
+        recyclerViewAdapter.addAll(liste);
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    private void loadData(int currentPage) {
+        CryptoAPI cryptoAPI = RetrofitClient.retrofitClient().create(CryptoAPI.class);
+        Call<Crypto> call = cryptoAPI.getData(40, "desc", currentPage);
+
+        call.enqueue(new Callback<Crypto>() {
+            @Override
+            public void onResponse(Call<Crypto> call, Response<Crypto> response) {
+                assert response.body() != null;
+                succesCode = response.code();
+                Crypto responseList = response.body();
+
+                for (int i = 0; i < responseList.getData().getCoins().size(); i++) {
+                    liste.add(responseList);
+                }
+
+                if (liste.size() > 0){
+
+                    showRecycler();
+                    showHideProgressHud(true);
+                    liste.clear();
+                    showHideProgressHud(false);
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Crypto> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+    }
+
+    private void loadNextPage(int currentPage) {
+        recyclerViewAdapter.removeLoadingFooter();
+        isLoading = false;
+        loadData(currentPage);
+        if (this.currentPage != TOTAL_PAGES) recyclerViewAdapter.addLoadingFooter();
+        else isLastPage = true;
+
+    }
+
+    private void showHideProgressHud(Boolean bool){
+        if (bool){
+            if (hud == null) {
+                hud = KProgressHUD.create(context)
+                        .setStyle(KProgressHUD.Style.SPIN_INDETERMINATE)
+                        .setLabel("Loading...")
+                        .setCancellable(true)
+                        .setAnimationSpeed(2)
+                        .setDimAmount(0.5f)
+                        .show();
+            }
+        }
+        else {
+
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    //Do something after 100ms
+                    if (hud != null) {
+                        hud.dismiss();
+                        hud = null;
+                    }
+                }
+            }, 1000);
+
+
+        }
+    }
+}
+
